848f2d00b3496e9fd121c8a52f0ab43144f90984
Alexandru Radu <radu.alexandru@expensya.com>
2024-02-09 16:23:33 +0200

    Refactor MeshRenderSystem to use Components namespace.

--- Nazar/Systems/Renderers/MeshRenderSystem.cs
+++ Nazar/Systems/Renderers/MeshRenderSystem.cs
@@ -1,25 +1,4 @@
-using Nazar.Components.Renderables;
-using Nazar.Foundation;
-
-namespace Nazar.Systems.Renderers;
-
-public class MeshRenderSystem : BaseSystem<float>
-{
-    public MeshRenderSystem(World world) : base(world)
-    {
-    }
-
-    public override void Update(float state)
-    {
-        var meshComponents = World.GetComponents<MeshComponent>();
-        foreach (var meshComponent in meshComponents)
-        {
-            meshComponent.Draw();
-        }
-    }
-}
-using Nazar.Components.Renderables;
-using Nazar.Foundation;
+using Nazar.Components;
 
 namespace Nazar.Systems.Renderers;
 
