fbbb63c6a4cfe1beb4074bc93c0b0d2295dfdc16
Alexandru Radu <radu.alexandru@expensya.com>
2024-02-09 16:20:15 +0200

    Add MeshRenderSystem for rendering mesh components.

---
+++ Nazar/Systems/Renderers/MeshRenderSystem.cs
@@ -0,0 +1,20 @@
+using Nazar.Components.Renderables;
+using Nazar.Foundation;
+
+namespace Nazar.Systems.Renderers;
+
+public class MeshRenderSystem : BaseSystem<float>
+{
+    public MeshRenderSystem(World world) : base(world)
+    {
+    }
+
+    public override void Update(float state)
+    {
+        var meshComponents = World.GetComponents<MeshComponent>();
+        foreach (var meshComponent in meshComponents)
+        {
+            meshComponent.Draw();
+        }
+    }
+}
