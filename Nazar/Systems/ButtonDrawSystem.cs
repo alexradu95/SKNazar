c17da7e119e96632739dbe90f5c04ff3cb011cc8
Alexandru Radu <radu.alexandru@expensya.com>
2024-02-09 14:59:08 +0200

    aider: Modified ButtonDrawSystem and TextWindowDrawSystem to draw entities at their positions if available, or at (0, 0, 0) if not.
    # Aider chat conversation:
    
    USER: Now I want to have a ButtonDrawSystem and a WindowDrawSystem that will check if it also has a PositionComponent, if it has it will draw them at the position, if not it will draw them at 0 0 0
    
    ASSISTANT: To implement this change, we need to modify the `ButtonDrawSystem.cs` and `TextWindowDrawSystem.cs` files.
    
    In both systems, we will modify the `Update` method to check if the entity has a `PositionComponent`. If it does, we will use that position to draw the entity. If it doesn't, we will draw the entity at the position (0, 0, 0).
    
    Here are the *SEARCH/REPLACE* blocks:
    
    Nazar\Systems\ButtonDrawSystem.cs
    <source>python
    <<<<<<< SEARCH
    public override void Update(float state)
    {
        var buttonsToDraw = World.GetEntities().With<DrawableComponent>().With<PositionComponent>().With<ButtonComponent>().With<TextContentsComponent>().AsSet();
    
        foreach (ref readonly var entity in buttonsToDraw.GetEntities())
        {
            ref var position = ref entity.Get<PositionComponent>();
            ref var button = ref entity.Get<ButtonComponent>();
            ref var text = ref entity.Get<TextContentsComponent>();
    
            UIHandler.DrawButton(ref button, ref position, ref text);
        }
    }
    =======
    public override void Update(float state)
    {
        var buttonsToDraw = World.GetEntities().With<DrawableComponent>().With<ButtonComponent>().With<TextContentsComponent>().AsSet();
    
        foreach (ref readonly var entity in buttonsToDraw.GetEntities())
        {
            ref var button = ref entity.Get<ButtonComponent>();
            ref var text = ref entity.Get<TextContentsComponent>();
            ref var position = ref entity.Get<PositionComponent>();
    
            if (entity.Has<PositionComponent>())
            {
                UIHandler.DrawButton(ref button, ref position, ref text);
            }
            else
            {
                var defaultPosition = new PositionComponent { Value = new Pose(0, 0, 0, Quat.Identity) };
                UIHandler.DrawButton(ref button, ref defaultPosition, ref text);
            }
        }
    }
    >>>>>>> REPLACE
    </source>
    
    Nazar\Systems\TextWindowDrawSystem.cs
    <source>python
    <<<<<<< SEARCH
    public override void Update(float state)
    {
        var windowsToDraw = World.GetEntities().With<DrawableComponent>().With<PositionComponent>().With<TextContentsComponent>().AsSet();
        foreach (ref readonly var entity in windowsToDraw.GetEntities())
        {
            ref var position = ref entity.Get<PositionComponent>();
            ref var textContent = ref entity.Get<TextContentsComponent>();
            UIHandler.DrawTextWindow(ref textContent, ref position);
        }
    }
    =======
    public override void Update(float state)
    {
        var windowsToDraw = World.GetEntities().With<DrawableComponent>().With<TextContentsComponent>().AsSet();
        foreach (ref readonly var entity in windowsToDraw.GetEntities())
        {
            ref var textContent = ref entity.Get<TextContentsComponent>();
    
            if (entity.Has<PositionComponent>())
            {
                ref var position = ref entity.Get<PositionComponent>();
                UIHandler.DrawTextWindow(ref textContent, ref position);
            }
            else
            {
                var defaultPosition = new PositionComponent { Value = new Pose(0, 0, 0, Quat.Identity) };
                UIHandler.DrawTextWindow(ref textContent, ref defaultPosition);
            }
        }
    }
    >>>>>>> REPLACE
    </source>

--- Nazar/Systems/ButtonDrawSystem.cs
+++ Nazar/Systems/ButtonDrawSystem.cs
@@ -7,15 +7,23 @@ public class ButtonDrawSystem(World world) : BaseSystem<float>(world)
 {
     public override void Update(float state)
     {
-        var buttonsToDraw = World.GetEntities().With<DrawableComponent>().With<PositionComponent>().With<ButtonComponent>().With<TextContentsComponent>().AsSet();
+        var buttonsToDraw = World.GetEntities().With<DrawableComponent>().With<ButtonComponent>().With<TextContentsComponent>().AsSet();
 
         foreach (ref readonly var entity in buttonsToDraw.GetEntities())
         {
-            ref var position = ref entity.Get<PositionComponent>();
             ref var button = ref entity.Get<ButtonComponent>();
             ref var text = ref entity.Get<TextContentsComponent>();
+            ref var position = ref entity.Get<PositionComponent>();
 
-            UIHandler.DrawButton(ref button, ref position, ref text);
+            if (entity.Has<PositionComponent>())
+            {
+                UIHandler.DrawButton(ref button, ref position, ref text);
+            }
+            else
+            {
+                var defaultPosition = new PositionComponent { Value = new Pose(0, 0, 0, Quat.Identity) };
+                UIHandler.DrawButton(ref button, ref defaultPosition, ref text);
+            }
         }
     }
 }
--- Nazar/Systems/TextWindowDrawSystem.cs
+++ Nazar/Systems/TextWindowDrawSystem.cs
@@ -10,12 +10,21 @@ public class TextWindowDrawSystem(World world) : BaseSystem<float>(world)
 {
     public override void Update(float state)
     {
-        var windowsToDraw = World.GetEntities().With<DrawableComponent>().With<PositionComponent>().With<TextContentsComponent>().AsSet();
+        var windowsToDraw = World.GetEntities().With<DrawableComponent>().With<TextContentsComponent>().AsSet();
         foreach (ref readonly var entity in windowsToDraw.GetEntities())
         {
-            ref var position = ref entity.Get<PositionComponent>();
             ref var textContent = ref entity.Get<TextContentsComponent>();
-            UIHandler.DrawTextWindow(ref textContent, ref position);
+
+            if (entity.Has<PositionComponent>())
+            {
+                ref var position = ref entity.Get<PositionComponent>();
+                UIHandler.DrawTextWindow(ref textContent, ref position);
+            }
+            else
+            {
+                var defaultPosition = new PositionComponent { Value = new Pose(0, 0, 0, Quat.Identity) };
+                UIHandler.DrawTextWindow(ref textContent, ref defaultPosition);
+            }
         }
     }
 }
