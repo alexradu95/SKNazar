990191b6939956004c86906d27b72803d2a94cb0
Alexandru Radu <radu.alexandru@expensya.com>
2024-02-09 14:25:03 +0200

    aider: Add another button that updates the second text window.
    # Aider chat conversation:
    
    USER: Add another button that will update the second text window
    
    ASSISTANT: To add another button that will update the second text window, we need to modify the `ApplicationInitializer.cs` file to create a new button and subscribe the second text window to its events.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    Nazar\Initialization\ApplicationInitializer.cs
    <source>
    <<<<<<< SEARCH
            EntityFactory.CreateButton(world, "Press Me!", new Pose(0.2f, 0, -0.5f, Quat.Identity));
            var textWindow1 = EntityFactory.CreateTextWindow(world, "test", new Pose(-0.2f, 0, -0.5f, Quat.Identity));
            textWindow1.Set(new SubscriberComponent { EventName = "ButtonPressed" });
            EntityFactory.CreateTextWindow(world, "new text window", new Pose(-0.4f, 0, -0.5f, Quat.Identity));
    =======
            var button1 = EntityFactory.CreateButton(world, "Press Me!", new Pose(0.2f, 0, -0.5f, Quat.Identity));
            button1.Set(new SubscriberComponent { EventName = "Button1Pressed" });
            var textWindow1 = EntityFactory.CreateTextWindow(world, "test", new Pose(-0.2f, 0, -0.5f, Quat.Identity));
            textWindow1.Set(new SubscriberComponent { EventName = "Button1Pressed" });
    
            var button2 = EntityFactory.CreateButton(world, "Press Me Too!", new Pose(0.4f, 0, -0.5f, Quat.Identity));
            button2.Set(new SubscriberComponent { EventName = "Button2Pressed" });
            var textWindow2 = EntityFactory.CreateTextWindow(world, "new text window", new Pose(-0.4f, 0, -0.5f, Quat.Identity));
            textWindow2.Set(new SubscriberComponent { EventName = "Button2Pressed" });
    >>>>>>> REPLACE
    </source>
    
    Please note that with this change, the first button will update the first text window and the second button will update the second text window. The `EventName` in the `SubscriberComponent` is used to distinguish between the events of the two buttons.

--- Nazar/Initialization/ApplicationInitializer.cs
+++ Nazar/Initialization/ApplicationInitializer.cs
@@ -34,9 +34,14 @@ public static class ApplicationInitializer
 
     private static void CreateEntities(World world)
     {
-        EntityFactory.CreateButton(world, "Press Me!", new Pose(0.2f, 0, -0.5f, Quat.Identity));
+        var button1 = EntityFactory.CreateButton(world, "Press Me!", new Pose(0.2f, 0, -0.5f, Quat.Identity));
+        button1.Set(new SubscriberComponent { EventName = "Button1Pressed" });
         var textWindow1 = EntityFactory.CreateTextWindow(world, "test", new Pose(-0.2f, 0, -0.5f, Quat.Identity));
-        textWindow1.Set(new SubscriberComponent { EventName = "ButtonPressed" });
-        EntityFactory.CreateTextWindow(world, "new text window", new Pose(-0.4f, 0, -0.5f, Quat.Identity));
+        textWindow1.Set(new SubscriberComponent { EventName = "Button1Pressed" });
+
+        var button2 = EntityFactory.CreateButton(world, "Press Me Too!", new Pose(0.4f, 0, -0.5f, Quat.Identity));
+        button2.Set(new SubscriberComponent { EventName = "Button2Pressed" });
+        var textWindow2 = EntityFactory.CreateTextWindow(world, "new text window", new Pose(-0.4f, 0, -0.5f, Quat.Identity));
+        textWindow2.Set(new SubscriberComponent { EventName = "Button2Pressed" });
     }
 }
