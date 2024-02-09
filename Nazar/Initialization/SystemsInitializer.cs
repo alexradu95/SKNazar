6f2d4498a46e73fb0c0e212cc5a62935477a9d63
Alexandru Radu <radu.alexandru@expensya.com>
2024-02-09 16:39:20 +0200

    aider: Added InitializeRenderingSystems method and updated InitializeSystems to include rendering systems.
    # Aider chat conversation:
    
    USER: Update the InitializeSystem, also add a method called InitializeRenderingSystems
    
    ASSISTANT: To update the `SystemsInitializer` class with a new method called `InitializeRenderingSystems`, I will add the method and refactor the `InitializeSystems` method to include a call to this new method. Here are the diffs for those changes:
    
    ```diff
    --- Nazar/Initialization/SystemsInitializer.cs
    +++ Nazar/Initialization/SystemsInitializer.cs
    @@ ... @@
     public static class SystemsInitializer
     {
    -    public static void InitializeSystems(World world, List<ISystem<float>> systems)
    +    public static void InitializeSystems(World world, List<ISystem<float>> systems, List<ISystem<float>> renderingSystems)
         {
             systems.Add(new ButtonRenderer(world));
             systems.Add(new ButtonInteractionSystem(world));
             systems.Add(new ButtonPressedMessageHandler(world));
    +        InitializeRenderingSystems(world, renderingSystems);
         }
    +
    +    public static void InitializeRenderingSystems(World world, List<ISystem<float>> renderingSystems)
    +    {
    +        renderingSystems.Add(new MeshRenderSystem(world));
    +        renderingSystems.Add(new ModelRenderSystem(world));
    +        renderingSystems.Add(new TextRenderSystem(world));
    +        renderingSystems.Add(new LineRenderSystem(world));
    +    }
     }
    ```
    
    These changes add a new method `InitializeRenderingSystems` to the `SystemsInitializer` class, which initializes rendering-related systems. The `InitializeSystems` method is also refactored to accept an additional parameter for rendering systems and to call the new method.

--- Nazar/Initialization/SystemsInitializer.cs
+++ Nazar/Initialization/SystemsInitializer.cs
@@ -4,10 +4,19 @@ namespace Nazar.Initialization;
 
 public static class SystemsInitializer
 {
-    public static void InitializeSystems(World world, List<ISystem<float>> systems)
+    public static void InitializeSystems(World world, List<ISystem<float>> systems, List<ISystem<float>> renderingSystems)
     {
         systems.Add(new ButtonRenderer(world));
         systems.Add(new ButtonInteractionSystem(world));
         systems.Add(new ButtonPressedMessageHandler(world));
+        InitializeRenderingSystems(world, renderingSystems);
+    }
+
+    public static void InitializeRenderingSystems(World world, List<ISystem<float>> renderingSystems)
+    {
+        renderingSystems.Add(new MeshRenderSystem(world));
+        renderingSystems.Add(new ModelRenderSystem(world));
+        renderingSystems.Add(new TextRenderSystem(world));
+        renderingSystems.Add(new LineRenderSystem(world));
     }
 }
