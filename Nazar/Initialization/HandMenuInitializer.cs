6f56a7f035d844243e779d6bb033746053f72307
Alexandru Radu <radu.alexandru@expensya.com>
2024-02-10 15:14:34 +0200

    Refactored HandMenuInitializer to create entities with specific components.

--- Nazar/Initialization/HandMenuInitializer.cs
+++ Nazar/Initialization/HandMenuInitializer.cs
@@ -12,12 +12,45 @@ public static class HandMenuInitializer
                 new HandMenuItem("Create", null, null, "Create"),
                 new HandMenuItem("Config", null, () => world.Get<EntityDebugSystem>().ToggleActive())),
             new HandRadialLayer("Create",
-                new HandMenuItem("Button", null, () => CreateButtonEntity(world)),
-                new HandMenuItem("Text Window", null, () => CreateTextWindowEntity(world)),
-                new HandMenuItem("Mesh", null, () => CreateMeshEntity(world)),
-                new HandMenuItem("Model", null, () => CreateModelEntity(world)),
-                new HandMenuItem("Text", null, () => CreateTextEntity(world)),
-                new HandMenuItem("Line", null, () => CreateLineEntity(world)),
+                new HandMenuItem("Button", null, () => {
+                    var entity = world.CreateEntity();
+                    entity.Set(new ButtonComponent { IsPressed = false });
+                    entity.Set(new TextComponent { Content = "New Button" });
+                    entity.Set(new TransformComponent { Position = new Pose(0.0f, 0, -0.5f, Quat.Identity) });
+                    entity.Set(new SubscriberComponent { EventName = "ButtonPressed" });
+                    entity.Set(new IdComponent(Guid.NewGuid()));
+                }),
+                new HandMenuItem("Text Window", null, () => {
+                    var entity = world.CreateEntity();
+                    entity.Set(new WindowComponent { Size = Vec2.Zero, ShowHeader = true });
+                    entity.Set(new TransformComponent { Position = new Pose(-0.3f, 0, -0.5f, Quat.Identity) });
+                    entity.Set(new SubscriberComponent { EventName = "ButtonPressed" });
+                    entity.Set(new IdComponent(Guid.NewGuid()));
+                }),
+                new HandMenuItem("Mesh", null, () => {
+                    var entity = world.CreateEntity();
+                    entity.Set(new MeshComponent { Mesh = Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f) });
+                    entity.Set(new TransformComponent { Position = new Pose(0.3f, 0, -0.5f, Quat.Identity) });
+                    entity.Set(new IdComponent(Guid.NewGuid()));
+                }),
+                new HandMenuItem("Model", null, () => {
+                    var entity = world.CreateEntity();
+                    entity.Set(new ModelComponent { Model = Model.FromFile("StereoKit.glb") });
+                    entity.Set(new TransformComponent { Position = new Pose(0.6f, 0, -0.5f, Quat.Identity) });
+                    entity.Set(new IdComponent(Guid.NewGuid()));
+                }),
+                new HandMenuItem("Text", null, () => {
+                    var entity = world.CreateEntity();
+                    entity.Set(new TextComponent { Content = "New Text" });
+                    entity.Set(new TransformComponent { Position = new Pose(-0.9f, 0, -0.5f, Quat.Identity) });
+                    entity.Set(new IdComponent(Guid.NewGuid()));
+                }),
+                new HandMenuItem("Line", null, () => {
+                    var entity = world.CreateEntity();
+                    entity.Set(new LineComponent { Start = new Vec3(-1.2f, 0, -0.5f), End = new Vec3(-1.2f, 0.1f, -0.5f) });
+                    entity.Set(new TransformComponent { Position = new Pose(-1.2f, 0, -0.5f, Quat.Identity) });
+                    entity.Set(new IdComponent(Guid.NewGuid()));
+                }),
                 new HandMenuItem("Back", null, null, HandMenuAction.Back))));
 
         SK.AddStepper(handMenu);
@@ -32,7 +65,7 @@ public static class HandMenuInitializer
         entity.Set(new IdComponent(Guid.NewGuid()));
     }
 
- 
+
 
     static void CreateMeshEntity(World world)
     {
